fdc291d948bae173667d1463bdf48551ced1a607
Longguang Wang <wanglongguang15@nudt.edu.cn>
2021-05-28 21:44:35 +0800

    Update quick_test.sh

--- quick_test.sh
+++ quick_test.sh
@@ -1,10 +1,10 @@
-# super-resolve a LR image (x2) using the model trained on noise-free degradations with isotropic Gaussian blurs
+# super-resolve an LR image (x2) using the model trained on noise-free degradations with isotropic Gaussian blurs
 python quick_test.py --img_dir='D:/LongguangWang/Data/test.png' \
                      --scale='2' \
                      --resume=600 \
                      --blur_type='iso_gaussian'
 
-# super-resolve a LR image (x4) using the model trained on general degradations with anisotropic Gaussian blurs and noises
+# super-resolve an LR image (x4) using the model trained on general degradations with anisotropic Gaussian blurs and noises
 python quick_test.py --img_dir='D:/LongguangWang/Data/test.png' \
                      --scale='4' \
                      --resume=600 \
